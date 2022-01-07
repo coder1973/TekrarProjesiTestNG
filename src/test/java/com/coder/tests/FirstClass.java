f4401e264ab86965b22a0b8d53418af9cf951fb1
coder1973 <bulentozturk2019@outlook.com>
2022-01-07 21:17:51 +0100

    add First.java

---
+++ src/test/java/com/coder/tests/FirstClass.java
@@ -0,0 +1,18 @@
+package com.coder.tests;
+
+import com.github.javafaker.Faker;
+
+public class FirstClass {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        Faker faker = new Faker();
+
+        System.out.println(faker.name().fullName());
+        System.out.println(faker.harryPotter().character());
+        System.out.println(faker.animal().name());
+    }
+
+
+}
